6ef7be5dec707670ff5dd256a26c508e2aa79196
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-05-16 18:21:08 -0500

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
+    if roman_string is None or type(roman_string) != str:
         return 0
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    roman_letters = [
-            ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
-            ['X', 10], ['V', 5], ['I', 1]
-    ]
-    num = 0
-    last = 0
+    sum = 0
 
-    for letter in roman_string:
-        for elem in roman_letters:
-            if letter == elem[0]:
-                if last == 0 or last >= elem[1]:
-                    num += elem[1]
-                elif last < elem[1]:
-                    num += elem[1] - (last * 2)
-
-                    last = elem[1]
-
-        return num
+    for i in range(len(roman_string)):
+        if i > 0 and rom_num[roman_string[i]] > rom_num[roman_string[i - 1]]:
+            sum += rom_num[roman_string[i]] - 2 * rom_num[roman_string[i - 1]]
+        else:
+            sum += rom_num[roman_string[i]]
+    return sum
